86ce67ccd21aa04f2c1d8db8bc2daecfd1e26fcb
Swayam Gaur <goura0775@gmail.com>
2026-02-01 23:28:37 +0530

    kjh

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import blogRoutes from "./routes/blogRoutes.js";
+import portfolioRoutes from "./routes/portfolioRoutes.js";
+import careerRoutes from "./routes/careerRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// static folder for images
+app.use("/uploads", express.static("uploads"));
+
+// ✅ Root route (this fixes Cannot GET /)
+app.get("/", (req, res) => {
+    res.send("Welcome to Backend API 🚀");
+});
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/portfolio", portfolioRoutes);
+// lkjh
+app.use("/api/career", careerRoutes);
+app.use("/api/contact", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
